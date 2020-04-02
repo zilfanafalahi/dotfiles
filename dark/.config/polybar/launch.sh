d73e324174478728ea032b0e5f9295b4ab361a5d
zilfanafalahi <zilfanafalahi45@gmail.com>
2020-04-02 14:34:37 +0700

    dotfiles dark mode

---
+++ dark/.config/polybar/launch.sh
@@ -0,0 +1,10 @@
+nate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar with name example
+polybar example -r &
+
+echo "Bars launched..."
